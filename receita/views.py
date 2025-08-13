a071120751be476b91d13f94280ecf40b30cd197
Rubens Gabriel <rugabriel.gabriel@gmail.com>
2025-08-13 13:57:18 -0300

    Próximos passos

--- receita/views.py
+++ receita/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Receita
 
 # Create your views here.
+def receitas(request):
+    receitas = Receita.objects.all()
+    context = {'receitas': receitas}
+
+    return render(request, 'minhas_receitas.html', context)
